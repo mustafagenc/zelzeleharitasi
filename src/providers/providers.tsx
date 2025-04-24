106bcfea3ce43fff3fc7392793916f04ea6a705e
Mustafa Genc <eposta@mustafagenc.info>
2025-04-24 23:44:24 +0300

    google analytics

--- src/providers/providers.tsx
+++ src/providers/providers.tsx
@@ -1,20 +1,28 @@
-"use client";
+'use client';
 
-import "@radix-ui/themes/styles.css";
+import '@radix-ui/themes/styles.css';
 
-import { Theme } from "@radix-ui/themes";
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
-import { ThemeProvider } from "next-themes";
+import { Theme } from '@radix-ui/themes';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import { ThemeProvider } from 'next-themes';
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  return (
-    <ThemeProvider attribute="class">
-      <Theme accentColor="blue" panelBackground="solid" radius="small">
-        {children}
-        <Analytics />
-        <SpeedInsights />
-      </Theme>
-    </ThemeProvider>
-  );
+	return (
+		<>
+			<ThemeProvider attribute='class'>
+				<Theme
+					accentColor='blue'
+					panelBackground='solid'
+					radius='small'
+				>
+					{children}
+					<Analytics />
+					<SpeedInsights />
+				</Theme>
+			</ThemeProvider>
+			<GoogleAnalytics gaId='G-JVB0DWQPQC' />
+		</>
+	);
 }
